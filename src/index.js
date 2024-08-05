a1de0ae327bfb1b0ec70211d9e9b2c54f13b9b42
petrushya <108490184+petrushya@users.noreply.github.com>
2024-08-05 11:12:23 +0300

    changes index files adds style, img

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+import '../assets/style.css';
+import brand from '../assets/brand.svg';
+
+const headerNode = document.querySelector('header');
+const logo = new Image();
+logo.src = brand;
+logo.alt = '';
+logo.width = '50';
+logo.height = '50';
+headerNode.appendChild(logo);
+const mainTitle = document.querySelector('main h1');
+const todaylink = document.querySelector('#linktoday');
+const basicElemNode = document.querySelector('section');
+mainTitle.textContent = 'today\'s list';
+basicElemNode.id = 'today';
+if(localStorage.getItem('jsonArticlesData')){
+  new createMenu(localStorage.getItem('jsonArticlesData')).menuProject;
+};
