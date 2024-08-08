291e91d85ea4d03a49a5ad09faf245ee0774191d
petrushya <108490184+petrushya@users.noreply.github.com>
2024-08-08 11:33:35 +0300

    changes parameters, methods in the class constructor in writearticles.js

--- data/writearticle.js
+++ data/writearticle.js
@@ -1,24 +1,39 @@
 export class articlesData {
-  constructor(projectName, dateTime, priority, contentArticle, noteArticle){
-    this.projectName = projectName ? projectName : "routine";
-    this.dateTime = dateTime ? new Date(dateTime).valueOf() : new Date().valueOf();
-    this.priority = priority ? priority : 'normal';
-    this.contentArticle = contentArticle ? contentArticle : 'empty';
-    this.noteArticle = noteArticle ? noteArticle : '';
+  constructor(projectName, dataArticle){
+    this.projectName = projectName.trim() ? projectName.trim() : 'routine';
+    this.dataArticle = dataArticle;
   }
+
+#articleArray(){
+  const dataArray = this.dataArticle.split('",,,"');
+  let articleArray = [];
+  if(dataArray[3].trim()){
+    const appArray = [
+      dataArray[0] ? dataArray[0] : new Date().valueOf().toString(),
+      dataArray[1] === 'checked' ? dataArray[1] : 'nonchecked',
+      dataArray[2] === 'low' || dataArray[2] === 'high' ? dataArray[2] : 'normal',
+      dataArray[3].trim(),
+      dataArray[4] ? dataArray[4] : ''
+    ];
+    articleArray = appArray.filter(item => item.length > 0);
+  };
+  return articleArray;
+}
+
   get addArticleData(){
-    const checkValue = 'nonchecked';
-    const totalObject = !localStorage.getItem('jsonArticlesData') ? Object() : JSON.parse(localStorage.getItem('jsonArticlesData'));
-    let articleArray = [this.dateTime.toString(),checkValue,this.priority,this.contentArticle,this.noteArticle].filter(item => item.length > 0);
-    if(!totalObject[this.projectName]){
-      totalObject[this.projectName] = [];
-    }else if(articleArray[3] === 'empty' && Object.keys(totalObject).includes(this.projectName)){
-      return;
+    const storageObject = !localStorage.getItem('todoList') ? Object() : JSON.parse(localStorage.getItem('todoList'));
+    if(!storageObject[this.projectName]) storageObject[this.projectName] = [];
+    if(this.#articleArray().length > 0){
+      storageObject[this.projectName].push(this.#articleArray().join('",,,"'));
+      storageObject[this.projectName].sort();
     };
-    if(articleArray[3] !== 'empty'){
-      totalObject[this.projectName].push(articleArray.join('%%%'));
-      totalObject[this.projectName].sort();
-    };
-    localStorage.setItem('jsonArticlesData', JSON.stringify(totalObject));
+    localStorage.setItem('todoList', JSON.stringify(storageObject));
+  }
+
+  get deletArticle(){
+    const storageObject = JSON.parse(localStorage.getItem('todoList'));
+    storageObject[this.projectName].splice(storageObject[this.projectName].indexOf(this.dataArticle), 1);
+    if(storageObject[this.projectName].length === 0) delete storageObject[this.projectName];
+    localStorage.setItem('todoList', JSON.stringify(storageObject));
   }
 }
