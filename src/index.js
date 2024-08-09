f551342adbbf4470fc4eba714a3a388157ca6453
petrushya <108490184+petrushya@users.noreply.github.com>
2024-08-09 18:06:05 +0300

    adds data for create loc...storage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import '../assets/style.css';
 import brand from '../assets/brand.svg';
-import { createMenu } from '../data/createmenu.js';
 import { articlesData } from '../data/writearticle.js';
 import { printContent } from '../data/printcontent.js';
 
 localStorage.clear();
 new articlesData(' ',['','','','daily job'].join('",,,"')).addArticleData;
-new articlesData('mydaily',['','','','some one'].join('",,,"')).addArticleData;
+new articlesData('mydaily',['','','','some one', 'lorem ipsum carent dolor:\n 1. something else'].join('",,,"')).addArticleData;
 new articlesData('mydaily',['','','','else some one'].join('",,,"')).addArticleData;
+new articlesData('mydaily',[`${new Date().valueOf() + 16*60*60*1000}`,'','','ooo some one'].join('",,,"')).addArticleData;
+new articlesData('mydaily',[new Date().valueOf() - 24*60*60*1000,'','','ooo else some one'].join('",,,"')).addArticleData;
 
 const headerNode = document.querySelector('header');
 const logo = new Image();
@@ -16,13 +17,10 @@ logo.alt = '';
 logo.width = '50';
 logo.height = '50';
 headerNode.appendChild(logo);
-const mainTitle = document.querySelector('main h1');
-const todayBtn = document.querySelector('[data-link="today"]');
-mainTitle.textContent = 'today\'s list';
-new createMenu(localStorage.getItem('todoList')).menuProject;
-new printContent('today').sectionContent;
+new printContent('today').pageContent;
 
+const todayBtn = document.querySelector('#todaylink');
 todayBtn.onclick = () => {
-  const pageLink = todayBtn.dataset.link;
-  new printContent(pageLink).sectionContent;
+  new printContent('today').pageContent;
+  todayBtn.blur();
 }
