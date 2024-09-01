e1c257ae1402629b59414a9c14067f1424f85370
petrushya <108490184+petrushya@users.noreply.github.com>
2024-09-01 22:23:35 +0300

    adds footer icon, styles and examples

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import '../assets/style.css';
 import brand from '../assets/brand.svg';
+import icon from '../assets/minima-social-icons.svg';
+import { reviseDate } from '../data/revisedate.js'; // to record examples
 import { articlesData } from '../data/writearticle.js';
 import { printContent } from '../data/printcontent.js';
 
 localStorage.clear();
-new articlesData('',[`${new Date().valueOf() - 30*60*1000}`,'','','daily job']).addArticleData;
-new articlesData('my lessons',[new Date().valueOf() + 24*60*60*1000,'normal','learn something from "javascrit"']).addArticleData;
-new articlesData('my lessons',[`${new Date().valueOf() - 60*60*1000}`,'','','some one', 'lorem ipsum carent dolor:\n 1. something else']).addArticleData;
-new articlesData('my lessons',[`${new Date().valueOf() - 90*60*1000}`,'','','new some one', 'lorem ipsum carent dolor:\n 1. something else\n2. else something else']).addArticleData;
-new articlesData('my lessons',[`${new Date().valueOf() + 60*60*1000}`,'','high','else else some one']).addArticleData;
-new articlesData('my lessons',[`${new Date().valueOf()}`,'','low','else some one']).addArticleData;
-new articlesData('my lessons',[`${new Date().valueOf() + 16*60*60*1000}`,'','high','ooo some one']).addArticleData;
-new articlesData('my lessons',[new Date().valueOf() - 24*60*60*1000,'','low','ooo else some one']).addArticleData;
+new articlesData('routine',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf() - 30*60*1000}`,'','','daily job']).addArticleData;
+new articlesData('my lessons',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf() + 24*60*60*1000}`,'normal','learn something from "javascrit"']).addArticleData;
+new articlesData('my lessons',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf() - 60*60*1000}`,'','','some one', 'lorem ipsum carent dolor:\n 1. something else']).addArticleData;
+new articlesData('my lessons',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf() - 90*60*1000}`,'','','new some one', 'lorem ipsum carent dolor:\n 1. something else\n2. else something else']).addArticleData;
+new articlesData('my lessons',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf() + 60*60*1000}`,'','high','else else some one']).addArticleData;
+new articlesData('my lessons',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf()}`,'','low','else some one']).addArticleData;
+new articlesData('my lessons',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf() + 16*60*60*1000}`,'','high','wow some one']).addArticleData;
+new articlesData('my lessons',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf() - 24*60*60*1000}`,'','low','wow else some one']).addArticleData;
+new articlesData('routine',[`${new Date(new reviseDate(Date()).sortFullDate).valueOf() + 2*24*60*60*1000}`,'','','future daily job']).addArticleData;
 
 const headerNode = document.querySelector('header');
 const logo = new Image();
@@ -20,12 +23,25 @@ logo.alt = '';
 logo.width = '50';
 logo.height = '50';
 headerNode.appendChild(logo);
-new printContent('today').pageContent;
+new printContent(new Date().getTime()).pageContent;
 
 const todayBtn = document.querySelector('#todaylink');
 todayBtn.onclick = () => {
   todayBtn.blur();
-  new printContent('today').pageContent;
+  new printContent(new Date().getTime()).pageContent;
 }
+
+const footerNode = document.querySelector('#footerContent');
+const link = document.createElement('a');
+link.setAttribute('href', 'https://github.com/petrushya/to-do');
+link.setAttribute('rel', 'noopener noreferrer');
+const p = document.createElement('p');
+const imgIcon = new Image;
+imgIcon.src = icon;
+imgIcon.width = '28';
+imgIcon.height ='28';
+footerNode.appendChild(link);
+link.appendChild(p);
+p.appendChild(imgIcon);
 
 
